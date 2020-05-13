e1873579c6a8418174fd4be910b9e3751040d747
amihut <amihut@EN610723>
2020-05-13 17:24:47 +0300

    fetching data and procesing it

--- main.js
+++ main.js
@@ -1,102 +1,219 @@
-var ctx = document.getElementById( 'pieChart' );
-var myPieChart = new Chart( ctx, {
-  type: 'pie',
-  data: {
-    labels: [ 'Carbs', 'Protein', 'Fat' ],
-    datasets: [ {
-      data: [ 12, 19, 3 ],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-      ],
-
-
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-      ],
-      borderWidth: 1
-    } ]
-  },
-  options: {
-    scales: {
-      yAxes: [ {
-        ticks: {
-          beginAtZero: true
-        }
-      } ]
-    }
-  }
-} );
-
-var ctx = document.getElementById( 'lineChart' );
-var myLineChart = new Chart( ctx, {
-  type: 'line',
-  data: {
-    labels: [ '02/13', '02/27', '03/12', '04/09', '04/23', '05/7' ],
-    datasets: [ {
-      fill: false,
-      label: 'Last 3 Months',
-      data: [ 12, 19, 3, 5, 2, 3 ],
-      backgroundColor: 'rgba(54, 162, 235, 1)',
-      borderColor: [
-        'rgba(54, 162, 235, 1)',
-      ],
-      borderWidth: 1
-    } ]
-  },
-  options: {
-    scales: {
-      yAxes: [ {
-        ticks: {
-          beginAtZero: true
-        }
-      } ]
-    }
-  }
-} );
-
 const macrosSelected = document.querySelector( '.macros-selected' );
 const kcalSelected = document.querySelector( '.kcal-selected' );
-const showCalendar = document.querySelector( '.show-calendar' );
-const submitDate = document.querySelector( '.date-picker__submit' );
 const addFood = document.querySelector( '.add-food' );
+const foodList = document.querySelector( '.search-list' );
+const foodListLogs = document.querySelector( '.food-list-logs' );
+const searchInput = document.querySelector( '.food-search__container__input' );
+const searchButton = document.querySelector( '.food-search__container__button' );
+const searchForm = document.querySelector( '.food-search__container' );
+let selectedFoods = [];
+let foodListData;
+let carbsData;
+let proteinData;
+let fatData;
+let kcalData = [];
+
 
 handleSelectMacros = () => {
   document.querySelector( '.chart-options__kcal' ).style.display = 'none';
   document.querySelector( '.chart-options__macros' ).style.display = 'block';
-  document.getElementById( 'lineChart' ).style.display = 'none';
-  document.getElementById( 'pieChart' ).style.display = 'block';
+  document.querySelector( '.line-chart' ).style.display = 'none';
+  document.querySelector( '.pie-chart' ).style.display = 'block';
 }
 
 handleSelectKcal = () => {
   document.querySelector( '.chart-options__macros' ).style.display = 'none';
   document.querySelector( '.chart-options__kcal' ).style.display = 'block';
-  document.getElementById( 'lineChart' ).style.display = 'block';
-  document.getElementById( 'pieChart' ).style.display = 'none';
+  document.querySelector( '.line-chart' ).style.display = 'block';
+  document.querySelector( '.pie-chart' ).style.display = 'none';
 }
 
-handleDisplayDatePicker = () => {
-  document.querySelector( '.date-picker' ).style.display = 'block';
+
+populateFoodList = ( data ) => {
+  let foodListItems = [];
+  data.map( ( item ) => {
+    foodListItems += `<button type="button" class="list-group-item list-group-item-action" data-dismiss="modal" data-id="${ item.id }">
+     <h5 class="search-list__item__title">${item[ 'food_name' ] }</h5>
+      <span class="search-list__item__content  d-flex justify-content-between">
+       <span class="description">
+         <span class="description__brand">${item[ 'brand_name' ] }</span>
+          <span class="description__quantity">
+            ${item[ 'quantity' ] }
+             <span class="description__quantity__units">g</span>
+           </span>
+          </span>
+          <span class="kcal">${item[ 'kcal' ] }</span>
+         </span>
+     </button>`
+  } )
+  foodList.innerHTML = foodListItems;
+
 }
 
-handleSubmitDate = ( e ) => {
-  e.preventDefault();
-  console.log( 'submitting date' );
+handleFetchData = () => {
+  const url = 'https://datastore-1ede4.firebaseio.com/foodList.json';
+  fetch( url ).then( function( response ) {
+    return response.json();
+  } ).then( function( data ) {
+    populateFoodList( data );
+    foodListData = [ ...data ]
+  } );
 }
 
 handleAddFood = () => {
-  console.log( 'add food' )
+  handleFetchData();
+}
+
+handlePopulateFoodListLogs = () => {
+  let foodListLogsItems = [];
+  selectedFoods.map( ( selectedFood ) => {
+    foodListLogsItems += `<li class="food-list-logs__item list-group-item">
+    <span class="food-name">${selectedFood[ 'food_name' ] }</span>
+    <ul class="text-center macros-list list-group list-group-horizontal">
+      <li class="list-group-item">
+        <div class="food-macros__kcal">
+          <span class="quantity">${selectedFood.kcal }</span>
+          <span class="quantity-unit">kcal</span>
+        </div>
+      </li>
+      <li class="list-group-item">
+        <div class="food-macros__carbs">
+          <span class="quantity">${selectedFood.carbs }</span>
+          <span class="quantity-unit">g</span>
+          <sub>Carbs</sub>
+        </div>
+      </li>
+      <li class="list-group-item">
+        <div class="food-macros__protein">
+          <span class="quantity">${selectedFood.protein }</span>
+          <span class="quantity-unit">g</span>
+          <sub>Protein</sub>
+        </div>
+      </li>
+      <li class="list-group-item">
+        <div class="food-macros__fat">
+          <span class="quantity">${selectedFood.fat }</span>
+          <span class="quantity-unit">g</span>
+          <sub>Fat</sub>
+        </div>
+      </li>
+    </ul>
+  </li>`;
+  } )
+
+  foodListLogs.innerHTML = foodListLogsItems;
+}
+
+renderPieChart = () => {
+  var ctx = document.getElementById( 'pieChart' );
+  var myPieChart = new Chart( ctx, {
+    type: 'pie',
+    data: {
+      labels: [ 'Carbs', 'Protein', 'Fat' ],
+      datasets: [ {
+        data: [ carbsData, proteinData, fatData ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+        ],
+
+
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      } ]
+    },
+    options: {
+      scales: {
+        yAxes: [ {
+          ticks: {
+            beginAtZero: true
+          }
+        } ]
+      }
+    }
+  } );
+}
+
+renderLineChart = () => {
+  var ctx = document.getElementById( 'lineChart' );
+  var myLineChart = new Chart( ctx, {
+    type: 'line',
+    data: {
+      labels: [ '02/13', '02/27', '03/12', '04/09', '04/23', '05/7' ],
+      datasets: [ {
+        fill: false,
+        label: 'Last 3 Months',
+        data: kcalData,
+        backgroundColor: 'rgba(54, 162, 235, 1)',
+        borderColor: [
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1
+      } ]
+    },
+    options: {
+      scales: {
+        yAxes: [ {
+          ticks: {
+            beginAtZero: true
+          }
+        } ]
+      }
+    }
+  } );
+}
+
+handleMacrosChartUpdate = () => {
+  carbsData = 0;
+  proteinData = 0;
+  fatData = 0;
+  kcalData = [];
+  selectedFoods.map( ( selectedFood ) => {
+    carbsData += selectedFood.carbs;
+    proteinData += selectedFood.protein;
+    fatData += selectedFood.fat;
+    kcalData = [ ...kcalData, selectedFood.kcal ];
+  } )
+
+  renderPieChart();
+  renderLineChart();
+}
+
+handleSelectFood = ( e ) => {
+  let { target } = e
+  let itemId;
+  while ( !itemId ) {
+    const targetId = target.dataset.id;
+    if ( targetId ) {
+      itemId = targetId;
+    } else {
+      target = target.parentNode;
+    }
+  }
+
+  selectedFoods = [ ...selectedFoods, ...foodListData.filter( ( item ) => item.id === itemId ) ]
+  handlePopulateFoodListLogs();
+  handleMacrosChartUpdate();
+  searchInput.value = '';
+}
+
+handleSearch = ( e ) => {
+  let results = foodListData.filter( ( foodData ) => {
+    return foodData[ 'food_name' ].toLowerCase().includes( searchInput.value.toLowerCase() );
+  } )
+  populateFoodList( results )
 }
 
 macrosSelected.addEventListener( 'click', handleSelectMacros );
 kcalSelected.addEventListener( 'click', handleSelectKcal );
-showCalendar.addEventListener( 'click', handleDisplayDatePicker );
-submitDate.addEventListener( 'click', ( e ) => handleSubmitDate( e ) );
 addFood.addEventListener( 'click', handleAddFood );
+foodList.addEventListener( 'click', ( e ) => handleSelectFood( e ) );
+searchInput.addEventListener( 'input', ( e ) => handleSearch( e ) )
 
-$( '#exampleModal' ).on( 'shown.bs.modal', function() {
-  $( '#myInput' ).trigger( 'focus' );
+$( '#addLogModal' ).on( 'shown.bs.modal', function() {
 } )
